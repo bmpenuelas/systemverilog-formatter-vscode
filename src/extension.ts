218822b417ea98dd19648f91a87c1dd7134f6659
Borja Penuelas <bmpenuelas@gmail.com>
2020-04-28 23:20:31 +0200

    Register language formatter for .sv and .svh

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,42 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { readFileSync } from "fs";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let rawFile = readFileSync("./verible_release_info.json");
+  let releaseInfo = JSON.parse(rawFile.toString());
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "systemverilog-formatter-vscode" is now active!');
+  vscode.languages.registerDocumentFormattingEditProvider("systemverilog", {
+    provideDocumentFormattingEdits(
+      document: vscode.TextDocument
+    ): vscode.TextEdit[] {
+      let filePath = document.uri.path;
+      console.log(releaseInfo);
+      console.log(filePath);
+      vscode.window.showInformationMessage("FORMAT");
+      return [];
+    },
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('systemverilog-formatter-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "systemverilog-formatter-vscode.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SystemVerilog code formatter &amp; linter!');
-	});
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from SystemVerilog!");
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
+
+  console.log("SVF ACTIVE");
 }
 
 // this method is called when your extension is deactivated
