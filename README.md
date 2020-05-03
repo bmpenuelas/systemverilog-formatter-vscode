48a99ea81ad597080e474cb1749f3a4218f662be
Borja Penuelas <bmpenuelas@gmail.com>
2020-05-03 03:26:07 +0200

    Working document formatting and selection formatting

--- README.md
+++ README.md
@@ -1,13 +1,45 @@
 # systemverilog-formatter-vscode README
 
-### SystemVerilog code formatting and linting in VSCode through Verible
+### SystemVerilog code formatting in VSCode through [google/Verible](https://github.com/google/verible)
 
-#### Supported Operative Systems
+### OS Support:
 
-As of now, Verible builds are only available for:
+As of now, Verible binaries are **only available for Linux**:
 
 - CentOS-8.1.1911-Core-x86_64
 - Ubuntu-14.04-trusty-x86_64
 - Ubuntu-16.04-xenial-x86_64
 - Ubuntu-18.04-bionic-x86_64
 - Ubuntu-19.10-eoan-x86_64
+
+Select which binary you want to use by setting `systemverilogFormatter.veribleBuild` in the Settings dropdown or in your `settings.json` as:
+
+```
+{
+  "systemverilogFormatter.veribleBuild": "Ubuntu-19.10-eoan-x86_64"
+}
+```
+
+The beginning of the string is used to select the version, so `Ubuntu-19`, `Ubuntu-19.10`, `Ubuntu-19.10-eoan`... are all valid values that will select `Ubuntu-19.10-eoan-x86_64`. Default: `Ubuntu-19.10`
+
+You can use this extension with **_Windows_, _Mac_... or any other OS** but in that case **you need to provide your own build of Verible** and make it accessible in your PATH. Then select `"none"` in the _veribleBuild_ dropdown in Settings, or add this to your `settings.json`:
+
+```
+{
+  "systemverilogFormatter.veribleBuild": "none"
+}
+```
+
+### Commands:
+
+#### Native formatting:
+
+This extension integrates with VSCode formatting, so you can use the built-in `Format Document`, format-on-save... and any linked functionality.
+
+#### Format document
+
+You can use the `SystemVerilog Formatter - Format this file` command from the command palette to apply formatting to the whole current document; or assign a Keyboard Shortcut to `systemverilog-formatter-vscode.formatDocument`
+
+#### Format document
+
+You can select a portion of the file and use the `SystemVerilog Formatter - Format selection` command from the command palette to apply formatting to just that section of the document; or assign a Keyboard Shortcut to `systemverilog-formatter-vscode.formatSelection`
--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,131 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
-import { readFileSync } from "fs";
+import * as vscode from 'vscode'
+import { readFileSync } from 'fs'
+import { join, dirname } from 'path'
+import * as child from 'child_process'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  let rawFile = readFileSync("./verible_release_info.json");
-  let releaseInfo = JSON.parse(rawFile.toString());
+// Constants
+const extensionID = 'bmpenuelas.systemverilog-formatter-vscode'
+const extensionPath = vscode.extensions.getExtension(extensionID)
+  ?.extensionPath as string
+const verible_release_info_path = join(
+  extensionPath,
+  '/src/',
+  'verible_release_info.json',
+)
+const extensionCfg = vscode.workspace.getConfiguration('systemverilogFormatter')
+const veribleReleaseInfo = JSON.parse(
+  readFileSync(verible_release_info_path).toString(),
+)
+const usedVeribleBuild = (() => {
+  if (extensionCfg.veribleBuild == '') return ''
+  if (extensionCfg.veribleBuild == 'none') return ''
+  for (const build in veribleReleaseInfo['release_subdirs']) {
+    let buildStr = veribleReleaseInfo['release_subdirs'][build]
+    if (buildStr.startsWith(extensionCfg.veribleBuild)) return buildStr
+  }
+})()
+const veribleBinPath = join(
+  extensionPath,
+  'src',
+  'verible_release',
+  usedVeribleBuild,
+  'verible-' + veribleReleaseInfo['tag'],
+  'bin',
+)
 
-  vscode.languages.registerDocumentFormattingEditProvider("systemverilog", {
-    provideDocumentFormattingEdits(
-      document: vscode.TextDocument
-    ): vscode.TextEdit[] {
-      let filePath = document.uri.path;
-      console.log(releaseInfo);
-      console.log(filePath);
-      vscode.window.showInformationMessage("FORMAT");
-      return [];
-    },
-  });
+// Get range of document
+const textRange = (document: vscode.TextDocument) =>
+  new vscode.Range(
+    document.lineAt(0).range.start,
+    document.lineAt(document.lineCount - 1).range.end,
+  )
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "systemverilog-formatter-vscode.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from SystemVerilog!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
-
-  console.log("SVF ACTIVE");
+// Format file
+const format = (
+  filePath: string,
+  documentText: string,
+  lines: Array<Array<number>> = [],
+  inPlace: boolean = false,
+) => {
+  let params = []
+  if (lines.length > 0)
+    params.push(
+      '--lines ' +
+        lines.map((range) => range.map((line) => line + 1).join('-')).join(','),
+    )
+  if (inPlace) params.push('--inplace')
+  let runLocation = dirname(filePath)
+  let command = [join(veribleBinPath, 'verilog_format'), ...params, '-'].join(
+    ' ',
+  )
+  let output = child.execSync(command, {
+    cwd: runLocation,
+    input: documentText,
+  })
+  return output.toString()
 }
 
-// this method is called when your extension is deactivated
+// Extension is activated
+export function activate(context: vscode.ExtensionContext) {
+  vscode.languages.registerDocumentFormattingEditProvider('systemverilog', {
+    provideDocumentFormattingEdits(
+      document: vscode.TextDocument,
+    ): vscode.TextEdit[] {
+      let filePath = document.uri.fsPath
+      let currentText = document.getText()
+      let formattedFileContents = format(filePath, currentText)
+
+      return [
+        vscode.TextEdit.replace(textRange(document), formattedFileContents),
+      ]
+    },
+  })
+
+  // Command: formatDocument
+  let formatDocument = vscode.commands.registerCommand(
+    'systemverilog-formatter-vscode.formatDocument',
+    () => {
+      var editor = vscode.window.activeTextEditor
+      if (editor) {
+        let document = editor.document as vscode.TextDocument
+        let filePath = document.uri.fsPath as string
+        let currentText = document.getText()
+        format(filePath, currentText)
+        editor.edit((editBuilder) =>
+          editBuilder.replace(
+            textRange(document),
+            format(filePath, currentText),
+          ),
+        )
+      }
+    },
+  )
+  context.subscriptions.push(formatDocument)
+
+  // Command: formatSelection
+  let formatSelection = vscode.commands.registerCommand(
+    'systemverilog-formatter-vscode.formatSelection',
+    () => {
+      var editor = vscode.window.activeTextEditor
+      if (editor) {
+        let document = editor.document as vscode.TextDocument
+        let currentText = document.getText()
+        let filePath = document.uri.fsPath as string
+        let selection = editor.selection
+        if (selection && !selection.isEmpty) {
+          let lines = [[selection.start.line, selection.end.line]]
+          editor.edit((editBuilder) =>
+            editBuilder.replace(
+              textRange(document),
+              format(filePath, currentText, lines),
+            ),
+          )
+        }
+      }
+    },
+  )
+  context.subscriptions.push(formatSelection)
+}
+
+// Extension is deactivated
 export function deactivate() {}
